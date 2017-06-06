1ca4db4c246ada6b33b7b5cbd756924843e381b5
Nicky Liu <nickyliu91@gmail.com>
2017-06-06 13:22:54 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,31 @@
+function forLoop(array) {
+  var newArray = array
+  for (var i = 0; i < 25; i++) {
+    if(i === 1) {
+      newArray.push("I am a strange loop.")
+    } else {
+      newArray.push( "I am ${i} strange loops.")
+    }
+  }
+  return newArray
+}
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(n)
+    n--
+  }
+  return 'done'
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(array) {
+  do {
+    array.pop()
+  } while (array.length > 0 && maybeTrue())
+  return array
+}
+
